1a1310363044fb6d07099776c982f2dc089bb434
Anuraag Barde <anuraagbarde@gmail.com>
2020-09-09 05:05:37 +0530

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+import os
+
+from django.contrib.auth.models import AbstractUser
+
+from s3direct.fields import S3DirectField
+from django.conf import settings
+
+from shortuuidfield import ShortUUIDField
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+class Student(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE, primary_key=True)
+    full_name = models.CharField(max_length=100,blank=False, null=False)
+    roll_no = models.IntegerField(unique=True)
+    join_year = models.IntegerField()
+    email = models.EmailField(unique=True)
+
+    student_created_time = models.DateField(auto_now=False, auto_now_add=True)
+    student_updated_time = models.DateField(auto_now=True, auto_now_add=False)
+
+    BCS = 'BCS'
+    IPG = 'IPG'
+    IMG = 'IMG'
+    COURSE_CHOICES = [
+        (BCS, 'BCS'),
+        (IPG, 'IPG'),
+        (IMG, 'IMG'),
+    ]
+    course = models.CharField(
+        max_length=3,
+        choices=COURSE_CHOICES,
+        blank=False, null=True
+    )
+
+
+    def __str__(self):
+        return self.full_name
+
+class Teacher(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE, primary_key=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    teacher_created_time = models.DateField(auto_now=False, auto_now_add=True)
+    teacher_updated_time = models.DateField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.full_name
+
+class Classroom(models.Model):
+    small_uuid = ShortUUIDField()
+    classroom_name = models.CharField(max_length=50, unique=True)
+    classroom_teacher = models.ForeignKey('User',on_delete=models.CASCADE, related_name='classroom_teachers')
+    classroom_student = models.ManyToManyField('User', related_name='classroom_students', blank=True)
+
+    classroom_created_time = models.DateField(auto_now=False, auto_now_add=True)
+    classroom_updated_time = models.DateField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.classroom_name
+
+class Video(models.Model):
+    video_title = models.CharField(max_length=40)
+    video_file = S3DirectField(unique=True,dest='primary_destination')
+    classroom = models.ForeignKey('Classroom', on_delete=models.CASCADE, related_name='videos')
+    comments = models.TextField(blank=True,default='')
+    video_created_time = models.DateField(auto_now=False, auto_now_add=True)
+    video_updated_time = models.DateField(auto_now=True, auto_now_add=False)
+
+    @property
+    def filename(self):
+        if self.video_file.startswith('http'):
+            cut = len(settings.AWS_S3_ENDPOINT_URL)+len('/') + len(settings.AWS_STORAGE_BUCKET_NAME)+len('/')
+            cut += len('input/')  # obtained from primary destination
+            return self.video_file[cut:]
+        else:
+            return self.video_file
+    
+    @property
+    def filename_without_extension(self):
+
+        filename_with_extension = self.filename
+        cut_file_extension_index = 0
+        for c in reversed(filename_with_extension):
+            cut_file_extension_index+=1
+            if c=='.':
+                break
+        if cut_file_extension_index<6:
+            return filename_with_extension[:cut_file_extension_index]
+        else:
+            return filename_with_extension
+
+    def __str__(self):
+        return self.filename_without_extension
+
---
+++ accounts/views.py
@@ -0,0 +1,142 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .decorators import teacher_required, student_required
+
+
+from django.views.generic import CreateView, ListView, UpdateView, DetailView, TemplateView, FormView
+
+from .models import *
+from .forms import *
+
+
+class TeacherSignUpView(CreateView):
+    model = User
+    form_class = TeacherSignUpForm
+    template_name = 'accounts/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'teacher'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('list_classrooms')
+
+
+class StudentSignUpView(CreateView):
+    model = User
+    form_class = StudentSignUpForm
+    template_name = 'accounts/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'student'
+        return super().get_context_data(**kwargs)
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('list_classrooms')
+
+@method_decorator([login_required, teacher_required], name='dispatch')
+class ClassroomCreateView(CreateView):
+    model = Classroom
+    form_class = ClassroomCreateForm
+    template_name = 'accounts/classroom_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_full_name'] = self.request.user.teacher.full_name
+        return super().get_context_data(**kwargs)
+    
+    def form_valid(self, form):
+        classroom_form = form.save(commit=False)
+        classroom_form.classroom_teacher = self.request.user
+        classroom_form.save()
+        return redirect('list_classrooms')
+
+
+@method_decorator([login_required], name='dispatch')
+class ClassroomListView(ListView):
+    model = Classroom
+    # ordering = ('classroom_name',)
+    context_object_name = 'classrooms'
+    template_name = 'accounts/classroom_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_teacher:
+            return self.request.user.classroom_teachers.all()
+        elif self.request.user.is_student:
+            return self.request.user.classroom_students.all()
+
+@method_decorator([login_required], name='dispatch')
+class ClassroomDetailView(DetailView):
+    model = Classroom
+    template_name='accounts/classroom_detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # if qs.exists():
+        if self.request.user.is_teacher:
+            return qs.filter(classroom_teacher=self.request.user)
+        elif self.request.user.is_student:
+            return qs.filter(classroom_student=self.request.user)
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['videos'] = context['object'].videos.all()
+        return context
+
+@method_decorator([login_required,student_required], name='dispatch')
+class ClassroomJoinView(FormView):
+    template_name = 'accounts/classroom_join.html'
+    form_class = ClassroomJoinForm
+
+    def form_valid(self, form):
+        unverified_code = form.cleaned_data['unverified_classcode']
+        qs = Classroom.objects.get(small_uuid = unverified_code)
+        qs.classroom_student.add(self.request.user)
+        return redirect('list_classrooms')
+    
+@method_decorator([login_required], name='dispatch')
+class VideoCreateView(CreateView):
+    model = Video
+    form_class = VideoCreateForm
+    template_name = 'accounts/video_form.html'
+
+    
+    def form_valid(self, form):
+        video_form = form.save(commit=False)
+        # video_form.classroom = self.request.
+        video_form.save()
+        return redirect('list_classrooms')
+
+@method_decorator([login_required], name='dispatch')
+class VideoDetailView(DetailView):
+    model = Video
+    template_name = 'accounts/video_detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # if qs.exists():
+        if self.request.user.is_teacher:
+            return qs.filter(classroom__classroom_teacher = self.request.user)
+        elif self.request.user.is_student:
+            return qs.filter(classroom__classroom_student = self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid_url = 'https://d35xdpmdsjhy8w.cloudfront.net/output/dash/' + context['object'].filename_without_extension + '.mpd'
+        context['video_url'] = vid_url
+        return context
+    
+
+
+def Home(request):
+    return HttpResponse('<html><p>this is home</p> </html>')
+
+def LogoutSuccessView(request):
+    return render(request,'registration/logout_successful.html' ,{})
